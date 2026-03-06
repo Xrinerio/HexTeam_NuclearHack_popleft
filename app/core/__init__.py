3f3cdc0274f0ba53dd9f27f7b094cdae861e0965
Lary_Stef <timurkotov1999@gmail.com>
2026-03-07 01:19:38 +0300

    refactor: remove message_parser from __all__ in __init__.py

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,6 +1,5 @@
 from . import utils
-from . import message_parser
 from .logger import logger
 from .settings import Settings
 
-__all__ = ["Settings", "logger", "message_parser", "utils"]
+__all__ = ["Settings", "logger", "utils"]
